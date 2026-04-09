184f7fed2f221b847830770764db2fce231ef862
Cuby92 <jakub.bartul@hotmail.com>
2026-04-09 13:29:37 +0000

    Add Footer styles, links and install SCSS

--- simerisracing/src/components/Footer/Footer.tsx
+++ simerisracing/src/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import styles from './Footer.module.css';
+import Link from 'next/link';
 
 const s = styles;
 
 function Footer() {
     return (
-        <footer>
-
+        <footer className={s.Footer}>
+            <Link className={`${s.a} a`} href='/'>Home Page</Link>
+            <Link className={`${s.a} a`} href='/about'>About</Link>
+            <Link className={`${s.a} a`} href='/products'>Products</Link>
+            <Link className={`${s.a} a`} href='/contact'>Contact</Link>
         </footer>
     );
 }
