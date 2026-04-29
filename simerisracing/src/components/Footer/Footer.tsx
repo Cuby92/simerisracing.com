735e21dc3b8ad1be787529248e78568b11b31183
Cuby92 <jakub.bartul@hotmail.com>
2026-04-29 17:25:50 +0200

    Fix Footer

--- simerisracing/src/components/Footer/Footer.tsx
+++ simerisracing/src/components/Footer/Footer.tsx
@@ -6,10 +6,12 @@ const s = styles;
 function Footer() {
     return (
         <footer className={s.Footer}>
-            <Link className={`${s.a} a`} href='/'>Home</Link>
-            <Link className={`${s.a} a`} href='/products'>Products</Link>
-            <Link className={`${s.a} a`} href='/about'>About</Link>
-            <Link className={`${s.a} a`} href='/contact'>Contact</Link>
+            <nav className={s.nav}>
+                <Link className={`${s.a} a`} href='/'>Home</Link>
+                <Link className={`${s.a} a`} href='/products'>Products</Link>
+                <Link className={`${s.a} a`} href='/about'>About</Link>
+                <Link className={`${s.a} a`} href='/contact'>Contact</Link>
+            </nav>
         </footer>
     );
 }
