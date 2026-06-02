a13bc03b33c38839f9a9c8455b51c2cb0f393153
Cuby92 <jakub.bartul@hotmail.com>
2026-06-02 13:47:27 +0200

    Make an offset correction

--- simerisracing/src/components/links/sidebar.tsx
+++ simerisracing/src/components/links/sidebar.tsx
@@ -16,7 +16,7 @@ function SidebarLink({ children, href, className, active = false, visited = fals
         <MotionLink
             href={href}
             className={`${className} ${s.sidebarLink}`}
-            style={ active ? { x: '2.2rem', scale: 1.01, fontWeight: 300 } : {} }
+            style={ active ? { x: '2.25rem', scale: 1.01, fontWeight: 300 } : {} }
             onMouseEnter={onMouseEnter}
         >
             { active ? <span style={{ color: '#f0f', transition: 'color 0.5s ease-in-out' }}>//</span> : <span style={{ color: '#f0f0', transition: 'color 0.5s ease-in-out' }}>//</span> } { children }
