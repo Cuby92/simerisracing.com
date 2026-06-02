ffe092026457cee7dc2eb2b481d0abb5f14394af
Cuby92 <jakub.bartul@hotmail.com>
2026-06-02 13:14:49 +0200

    Add fonts

--- simerisracing/src/app/layout.tsx
+++ simerisracing/src/app/layout.tsx
@@ -2,16 +2,27 @@ import "./globals.scss";
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
 import CursorCssVars from '@/utils/cursorPosition';
-import { Raleway } from 'next/font/google';
+import { Raleway, Roboto_Flex, Poppins } from 'next/font/google';
 
 const raleway = Raleway({
   subsets: ['latin'],
   variable: '--raleway'
 });
 
+const robotoFlex = Roboto_Flex({
+  subsets: ['latin'],
+  variable: '--roboto-flex'
+});
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
 function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html className={raleway.variable} lang="en">
+    <html className={`${raleway.variable} ${robotoFlex.className} ${poppins.variable}`} lang="en">
       <CursorCssVars />
       <body>
         <NavBar />
