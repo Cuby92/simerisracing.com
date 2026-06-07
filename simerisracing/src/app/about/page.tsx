fda448dcc274e17252864fa279cae50e8834d027
Cuby92 <jakub.bartul@hotmail.com>
2026-06-07 15:08:28 +0200

    Create About page under construction

---
+++ simerisracing/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.scss';
+
+const s = styles;
+
+function About() {
+    return (
+        <h1 className={s.h1}>Page under construction...</h1>
+    )
+}
+
+export default About;
