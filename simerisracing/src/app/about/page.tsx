40121c4d8680bf7bddc3217a57f15f548d74ce9b
Cuby92 <jakub.bartul@hotmail.com>
2026-06-18 17:34:45 +0200

    Add Our Philosophy section

--- simerisracing/src/app/about/page.tsx
+++ simerisracing/src/app/about/page.tsx
@@ -10,11 +10,13 @@ function About() {
                 <h1 className={s.bgText}><span className={s.slashes}>//</span>About Us</h1>
                 <h1 className={s.h1}>About us</h1>
                 <p className={s.p}>We are a small team with a simple goal: build sim racing gear that actually makes sense. <strong className={s.bold}>Durable hardware, thoughtful engineering, and long-term support</strong> - shaped by people who race everyday.</p>
-                <ScrollDownButton nextSection={'#intro'} />
+                <ScrollDownButton nextSection={'#our-philosophy'} />
             </section>
 
-            <section className={`${s.IntroSection} page`} id="intro">
-                <ScrollDownButton nextSection={''} />
+            <section className={`${s.PhilosophySection} page`} id="our-philosophy">
+                <ScrollDownButton nextSection={'#our-commitment'} />
+                <h2 className={s.h2}>Our Philosophy</h2>
+                <p>The most important factor for us is the customer satisfaction - we don't just want to sell a product and disappear. We want people to be fully satisfied with their purchase. In the design process we focus on durability, realism, and performance. We also don't make loud marketing like big brands - we focus on building a loyal, supportive community.</p>
             </section>
         </div>
     );
