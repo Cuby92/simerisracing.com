f2c1ca43db9ee66832054e0cb9cc91c477836814
Cuby92 <jakub.bartul@hotmail.com>
2026-06-18 17:19:38 +0200

    Create page 1

--- simerisracing/src/app/about/page.tsx
+++ simerisracing/src/app/about/page.tsx
@@ -1,13 +1,23 @@
 import styles from './page.module.scss';
+import ScrollDownButton from '@/components/ScrollDownButton/ScrollDownButton';
 
 const s = styles;
 
 function About() {
     return (
-        <section className="page">
-            <h1 className={s.h1}>Page under construction...</h1>   
-        </section>
-    )
+        <div className={s.Page}>
+            <section className={`${s.HeroSection} page`}>
+                <h1 className={s.bgText}><span className={s.slashes}>//</span>About Us</h1>
+                <h1 className={s.h1}>About us</h1>
+                <p className={s.p}>We are a small team with a simple goal: build sim racing gear that actually makes sense. <strong className={s.bold}>Durable hardware, thoughtful engineering, and long-term support</strong> - shaped by people who race everyday.</p>
+                <ScrollDownButton nextSection={'#intro'} />
+            </section>
+
+            <section className={`${s.IntroSection} page`} id="intro">
+                <ScrollDownButton nextSection={''} />
+            </section>
+        </div>
+    );
 }
 
 export default About;
