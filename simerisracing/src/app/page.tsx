37eb3e173f76573e3d6f205dcb5bbc5ee9e84013
Cuby92 <jakub.bartul@hotmail.com>
2026-07-02 11:31:57 +0200

    Add menu on Home page

--- simerisracing/src/app/page.tsx
+++ simerisracing/src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from "./page.module.scss";
 import CarbonBg from '@/components/CarbonBg/CarbonBg';
 import ScrollSmootherWrapper from '@/utils/gsap/ScrollSmoother';
 import Footer from '@/components/Footer/Footer';
+import StdLink from '@/components/links/std/StdLink';
 
 const s = styles;
 
@@ -9,8 +10,16 @@ function Home() {
     return (
         <ScrollSmootherWrapper>
             <CarbonBg />
-            <div className="page">
-                <h1 className={s.h1}>Page under construction...</h1>
+            <div className={`page ${s.Cover}`}>
+                <h1>Page under construction...</h1>
+                <nav>
+                    <h2>This page is not yet available. Try visiting another pages instead:</h2>
+                    <ul>
+                        <li><StdLink href="/about">About</StdLink></li>
+                        <li><StdLink href="/contact">Contact</StdLink></li>
+                        <li><StdLink href="/products/panther-hp">PANTHER HP</StdLink></li>
+                    </ul>
+                </nav>
             </div>
             <Footer />
         </ScrollSmootherWrapper>
